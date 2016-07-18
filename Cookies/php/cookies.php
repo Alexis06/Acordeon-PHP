763d0db7f0fddb01b810d684b1bc65eadbdd4432
Alexis <brian.bobi@outlook.com>
2016-07-18 17:20:03 -0500

    Añadi estilos a todos los archivos de la Carpeta de Cookies

--- Cookies/php/cookies.php
+++ Cookies/php/cookies.php
@@ -1,6 +1,6 @@
 <?php
 //Las cookies van hasta arriba del código SIEMPRE!
-/*Los argumentos que necesita el cookie van dentro de la función "setcookie" y los parametros son nombre, valor, duración (la duración es por segundos), ruta, dominio, seguridad, separados por ","*/ 
+/*Los argumentos que necesita el cookie van dentro de la función "setcookie" y los parametros son nombre, valor, duración (la duración es por segundos), ruta, dominio, seguridad, separados por ","*/
 setcookie('user','Alexis');
   ?>
 <!DOCTYPE html>
@@ -8,17 +8,19 @@ setcookie('user','Alexis');
   <head>
     <meta charset="utf-8">
     <title>Cookie</title>
+    <link href="../css/style.css" rel="stylesheet" type="text/css" media="screen">
   </head>
   <body>
 
 <?php
-
+print('<div class="resultado">');
 if(isset ($_COOKIE['user'] ) ){
   print('El valor de esta cookie es: <br>');
   print($_COOKIE['user']);
 } else{
   print('Aun no se define la cookie, debes de recargar la página.');
 }
+print('</div>');
 ?>
   </body>
 </html>
