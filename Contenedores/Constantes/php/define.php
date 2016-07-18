6f6a1386c0246cadbc38e32465d50242e527de83
Alexis <brian.bobi@outlook.com>
2016-07-18 16:41:40 -0500

    Agregue estilos a los archivos de la Carpeta de Constantes

--- Contenedores/Constantes/php/define.php
+++ Contenedores/Constantes/php/define.php
@@ -3,13 +3,17 @@
   <head>
     <meta charset="utf-8">
     <title>Define</title>
+    <link href="../css/style.css" rel="stylesheet" type="text/css" media="screen">
   </head>
   <body>
-
-    <?php
-      //Son varibles que nunca cambiaran su valor
-      define('nombreAutor', 'Alexis');/*Los strings van entre comillas sin importar si es el nombre de la constante, pero al imprimir solo se pondra el nombre de la constante (sin comillas). EL PRIMER CAMPO ES EL NOMBRE DE LA CONSTANTE*/
-      print('El Autor de este codigo es: ' .nombreAutor);
-    ?>
+      <?php
+        //Son varibles que nunca cambiaran su valor
+        print('<div class="letras">');
+        print('<p>');
+        define('nombreAutor', 'Alexis');/*Los strings van entre comillas sin importar si es el nombre de la constante, pero al imprimir solo se pondra el nombre de la constante (sin comillas). EL PRIMER CAMPO ES EL NOMBRE DE LA CONSTANTE*/
+        print('El Autor de este codigo es: ' .nombreAutor);
+        print('</p>
+        </div>');
+      ?>
   </body>
 </html>
