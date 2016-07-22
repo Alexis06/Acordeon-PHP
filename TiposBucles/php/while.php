8db00de3da6a9875260a622d5fe14f6ca9e7115b
Alexis <brian.bobi@outlook.com>
2016-07-22 14:38:38 -0500

    Bucle While como tal

---
+++ TiposBucles/php/while.php
@@ -0,0 +1,19 @@
+<?php
+#Bucle While
+//El Bucle While es el que trabaja con valores Booleanos y se utiliza cuando queremos iniciar un ciclo dependiente a una posibilidad.
+
+/*Su sintaxis es la siguiente:
+Se inicia el while, dentro de los parentesis iran dos funciones: 'list' y 'each'.
+list, representa y/o es 
+*/
+while (list () = each()){
+
+
+}
+
+
+
+
+
+
+?>
