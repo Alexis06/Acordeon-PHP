bb04c73170dcbe1ce16401605c0121168fbec75e
Alexis <brian.bobi@outlook.com>
2016-07-23 11:52:24 -0500

    Do While

---
+++ TiposBucles/php/do_while.php
@@ -0,0 +1,13 @@
+<?php
+#Bucle While
+//Se utiliza menos que While y al contrario de While, Do while evalua el final.
+/*En la sintaxis solo se agrega el Do y se abren llaves pero en el while solo se declaran los datos que se quieren evaluar y no se abren las llaves como en el While normal*/
+$numero = 0;
+
+do{
+
+      print($numero++);
+
+}
+while ($numero<10);
+?>
